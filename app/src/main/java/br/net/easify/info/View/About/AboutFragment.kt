a207015d728d12b905746126eeffa36a3a9cf896
viniciuspontes <vipontes70@gmail.com>
2019-09-15 07:53:53 -0300

    About fragment implementation

--- app/src/main/java/br/net/easify/info/View/About/AboutFragment.kt
+++ app/src/main/java/br/net/easify/info/View/About/AboutFragment.kt
@@ -1,12 +1,16 @@
 package br.net.easify.info.View.About
 
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import br.net.easify.info.R
+import kotlinx.android.synthetic.main.activity_news_detail.*
+import kotlinx.android.synthetic.main.fragment_about.*
 
 
 class AboutFragment : Fragment() {
@@ -17,5 +21,22 @@ class AboutFragment : Fragment() {
     ): View? {
 
         return inflater.inflate(R.layout.fragment_about, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        linkedin.setOnClickListener(View.OnClickListener {
+            val openURL = Intent(Intent.ACTION_VIEW)
+            openURL.data = Uri.parse("https://www.linkedin.com/in/vinicius-pontes-8b152383/")
+            startActivity(openURL)
+        })
+
+        github.setOnClickListener(View.OnClickListener {
+            val openURL = Intent(Intent.ACTION_VIEW)
+            openURL.data = Uri.parse("https://github.com/vipontes")
+            startActivity(openURL)
+        })
     }
 }
